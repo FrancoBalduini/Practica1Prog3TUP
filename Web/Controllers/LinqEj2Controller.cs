8c979eddbf1d9799630021da96c6bbe11d1b00ff
FrancoBalduini <franco233433@gmail.com>
2024-04-10 14:22:08 -0300

    Linq ejercicio 2 resuelto

--- Web/Controllers/LinqEj2Controller.cs
+++ Web/Controllers/LinqEj2Controller.cs
@@ -1,6 +1,19 @@
-﻿namespace Web.Controllers
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
 {
-    public class LinqEj2Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2LinqController : ControllerBase
     {
+        [HttpGet("consulta2")]
+        public List<string> Consulta2([FromQuery] List<string> words)
+        {
+            var result = words.Where(word => word.Length >= 5).Select(word => word.ToUpper()).ToList();
+            return result;
+        }
     }
 }
